9fb8d81f930a8444684e9f5bacfe4ed76a594f2e
Bryan Jonas <53985683+bryanjonas@users.noreply.github.com>
2019-11-05 13:44:24 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='jupyterhub-repo2dockerspawner',
+    name='wpcdas-repo2dockerspawner',
     version='0.1',
     packages=find_packages(),
     install_requires=[
