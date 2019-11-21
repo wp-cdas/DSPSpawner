ffeba269e4915ea66a103244429e917290813a2a
Bryan Jonas <53985683+bryanjonas@users.noreply.github.com>
2019-11-21 08:32:54 -0500

    Update __init__.py

--- dspspawner/__init__.py
+++ dspspawner/__init__.py
@@ -92,7 +92,7 @@ class DSPProfilesSpawner(ProfilesSpawner):
     profiles = List(
         trait = Tuple( Unicode(), Unicode(), Type(Spawner), Dict() ),
         default_value = [ ( 'Normal Environment', 'singleuser', 'dspspawner.DSPSwarmSpawner',
-                            dict(image = 'cdasdsp/datasci-rstudio-notebook:2',
+                            dict(image = 'cdasdsp/datasci-rstudio-notebook:3',
                             volumes = {'/mnt/data':'/data'},
                             network_name = network_name,
                             remove_containers = True,
